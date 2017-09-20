0418dfde6f7d9fa331f5702972048c7f35bdbf33
enixlin <enixlin@126.com>
2017-09-20 12:43:29 +0800

    add three.js lib

--- views/login/index-random.php
+++ views/login/index-random.php
@@ -33,22 +33,16 @@
 <body>
 
 
-        <script src="/jrrc_web_dev/web/build/three.js"></script>
+<script src="/jrrc_web_dev/web/build/three.js"></script>
 
 		<script src="/jrrc_web_dev/web/js/renderers/Projector.js"></script>
 		<script src="/jrrc_web_dev/web/js/renderers/CanvasRenderer.js"></script>
-        <script src="/jrrc_web_dev/web/js/libs/stats.min.js"></script>
-        
 
-        <script src="js/renderers/Projector.js"></script>
-		<script src="js/renderers/CanvasRenderer.js"></script>
-
-		<script src="js/libs/stats.min.js"></script>
-		<script src="js/libs/tween.min.js"></script>
+		<script src="/jrrc_web_dev/web/js/libs/stats.min.js"></script>
 
 		<script>
 			var container, stats;
-			var camera, scene, renderer, particle;
+			var camera, scene, renderer, group, particle;
 			var mouseX = 0, mouseY = 0;
 			var windowHalfX = window.innerWidth / 2;
 			var windowHalfY = window.innerHeight / 2;
@@ -57,18 +51,28 @@
 			function init() {
 				container = document.createElement( 'div' );
 				document.body.appendChild( container );
-				camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 5000 );
+				camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 3000 );
 				camera.position.z = 1000;
 				scene = new THREE.Scene();
-				scene.background = new THREE.Color( 0x000040 );
-				var material = new THREE.SpriteMaterial( {
-					map: new THREE.CanvasTexture( generateSprite() ),
-					blending: THREE.AdditiveBlending
-				} );
+				var PI2 = Math.PI * 2;
+				var program = function ( context ) {
+					context.beginPath();
+					context.arc( 0, 0, 0.5, 0, PI2, true );
+					context.fill();
+				};
+				group = new THREE.Group();
+				scene.add( group );
 				for ( var i = 0; i < 1000; i++ ) {
+					var material = new THREE.SpriteCanvasMaterial( {
+						color: Math.random() * 0x808008 + 0x808080,
+						program: program
+					} );
 					particle = new THREE.Sprite( material );
-					initParticle( particle, i * 10 );
-					scene.add( particle );
+					particle.position.x = Math.random() * 2000 - 1000;
+					particle.position.y = Math.random() * 2000 - 1000;
+					particle.position.z = Math.random() * 2000 - 1000;
+					particle.scale.x = particle.scale.y = Math.random() * 20 + 10;
+					group.add( particle );
 				}
 				renderer = new THREE.CanvasRenderer();
 				renderer.setPixelRatio( window.devicePixelRatio );
@@ -89,53 +93,20 @@
 				camera.updateProjectionMatrix();
 				renderer.setSize( window.innerWidth, window.innerHeight );
 			}
-			function generateSprite() {
-				var canvas = document.createElement( 'canvas' );
-				canvas.width = 16;
-				canvas.height = 16;
-				var context = canvas.getContext( '2d' );
-				var gradient = context.createRadialGradient( canvas.width / 2, canvas.height / 2, 0, canvas.width / 2, canvas.height / 2, canvas.width / 2 );
-				gradient.addColorStop( 0, 'rgba(255,255,255,1)' );
-				gradient.addColorStop( 0.2, 'rgba(0,255,255,1)' );
-				gradient.addColorStop( 0.4, 'rgba(0,0,64,1)' );
-				gradient.addColorStop( 1, 'rgba(0,0,0,1)' );
-				context.fillStyle = gradient;
-				context.fillRect( 0, 0, canvas.width, canvas.height );
-				return canvas;
-			}
-			function initParticle( particle, delay ) {
-				var particle = this instanceof THREE.Sprite ? this : particle;
-				var delay = delay !== undefined ? delay : 0;
-				particle.position.set( 0, 0, 0 );
-				particle.scale.x = particle.scale.y = Math.random() * 32 + 16;
-				new TWEEN.Tween( particle )
-					.delay( delay )
-					.to( {}, 10000 )
-					.onComplete( initParticle )
-					.start();
-				new TWEEN.Tween( particle.position )
-					.delay( delay )
-					.to( { x: Math.random() * 4000 - 2000, y: Math.random() * 1000 - 500, z: Math.random() * 4000 - 2000 }, 10000 )
-					.start();
-				new TWEEN.Tween( particle.scale )
-					.delay( delay )
-					.to( { x: 0.01, y: 0.01 }, 10000 )
-					.start();
-			}
 			//
 			function onDocumentMouseMove( event ) {
 				mouseX = event.clientX - windowHalfX;
 				mouseY = event.clientY - windowHalfY;
 			}
 			function onDocumentTouchStart( event ) {
-				if ( event.touches.length == 1 ) {
+				if ( event.touches.length === 1 ) {
 					event.preventDefault();
 					mouseX = event.touches[ 0 ].pageX - windowHalfX;
 					mouseY = event.touches[ 0 ].pageY - windowHalfY;
 				}
 			}
 			function onDocumentTouchMove( event ) {
-				if ( event.touches.length == 1 ) {
+				if ( event.touches.length === 1 ) {
 					event.preventDefault();
 					mouseX = event.touches[ 0 ].pageX - windowHalfX;
 					mouseY = event.touches[ 0 ].pageY - windowHalfY;
@@ -148,15 +119,14 @@
 				stats.update();
 			}
 			function render() {
-				TWEEN.update();
 				camera.position.x += ( mouseX - camera.position.x ) * 0.05;
 				camera.position.y += ( - mouseY - camera.position.y ) * 0.05;
 				camera.lookAt( scene.position );
+				group.rotation.x += 0.01;
+				group.rotation.y += 0.02;
 				renderer.render( scene, camera );
 			}
 		</script>
-
-		
 
 
 
